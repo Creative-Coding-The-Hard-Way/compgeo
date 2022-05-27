db1d6ed00d1a4a088c28b4c82d241af3b8cd405e
Bradley Lyman <lyman.brad3211@gmail.com>
2022-05-26 22:50:30 -0700

    add diagram note to readme

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # compgeo
 Data structures and algorithms for common computation geometry problems in 2D.
+
+## Appendix A - Diagrams
+
+Documentation in this repo uses ASCII diagrams generated using this site:
+https://textik.com/
--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,6 @@
 //! This crate provides data structures and algorithms for dealing with common
 //! computational geometry problems.
 //!
-//! Generate diagrams with ASCII art here: <https://textik.com/>
-//!
-//! ```none
-//! +----------------------------------------------------+
-//! |    ^                                               |
-//! |    +                    +                          |
-//! |    |                                               |
-//! |    +                 +                             |
-//! |    |                                               |
-//! |    +              +                                |
-//! |    |                                               |
-//! |    +           +                                   |
-//! |    |                                               |
-//! |    +        +                                      |
-//! |    |                                               |
-//! |    +     +                                         |
-//! |    |                                               |
-//! |    +  +                                            |
-//! |    |                                               |
-//! |x---+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+> |
-//! |    |                                               |
-//! |  y |                                               |
-//! +----------------------------------------------------+
-//! ```
 
 pub mod line;
 pub mod operations;
